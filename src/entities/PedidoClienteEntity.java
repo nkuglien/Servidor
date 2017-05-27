129a32d1f42c19b8358cb1a16978da78250cc2fd
Ezequiel Maranga <ezequiel.maranga@synapse.com>
2017-05-27 13:34:13 -0300

    mapeo del enum

--- src/entities/PedidoClienteEntity.java
+++ src/entities/PedidoClienteEntity.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -58,6 +60,8 @@ public class PedidoClienteEntity {
 	@Column(name = "total")
 	private Float total;
 	
+	@Column(name="estado") 
+	@Enumerated(EnumType.STRING) 
 	private EstadoPedidoCliente estado;
 	
 	@Column(name = "nota")
