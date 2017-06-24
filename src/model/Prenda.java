01f5445aa9adadc609d3d8f0ef311e977a10e57b
Ivan Addolorato <Coco@Mac-mini-de-Ivan.local>
2017-06-24 17:17:34 -0300

    cambos para alta variedad prenda y alta prenda

--- src/model/Prenda.java
+++ src/model/Prenda.java
@@ -127,7 +127,7 @@ public class Prenda {
 		
 		List<PrendaAreaProduccionDTO> areasDTO = new ArrayList<PrendaAreaProduccionDTO>();
 		for(PrendaAreaProduccion area : this.getAreas()) {
-			areas.add(area.toDTO());
+			areasDTO.add(area.toDTO());
 		}
 		dto.setAreas(areasDTO);
 		return dto;
